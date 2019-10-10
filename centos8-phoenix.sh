dfadb1719c4d855ef9dfaf024673f09cf4c3ec98
alexlllll <alexlllll>
2019-10-10 22:40:39 +0200

    inotify-tools

--- centos8-phoenix.sh
+++ centos8-phoenix.sh
@@ -10,7 +10,7 @@ scratchmnt=$(buildah mount ${newcontainer})
 buildah config --author "https://github.com/alexlllll" "$newcontainer"
 
 # install the packages
-buildah unshare dnf install --installroot ${scratchmnt} bash coreutils glibc-langpack-en microdnf centos-release unzip \
+dnf install --installroot ${scratchmnt} bash coreutils glibc-langpack-en microdnf centos-release unzip \
 	--disablerepo="*" \
 	--enablerepo=BaseOS \
 	--releasever 8 \
@@ -19,11 +19,12 @@ buildah unshare dnf install --installroot ${scratchmnt} bash coreutils glibc-lan
 	-y \
 	/	
 
-buildah run "${newcontainer}" -- microdnf install -y epel-release
-buildah run "${newcontainer}" -- microdnf install -y --enablerepo PowerTools erlang
+buildah run "${newcontainer}" -- microdnf install --nodocs -y epel-release 
+buildah run "${newcontainer}" -- microdnf install --nodocs -y --enablerepo PowerTools erlang
 buildah run "${newcontainer}" -- mkdir /opt/elixir 
 buildah run "${newcontainer}" -- curl -L https://github.com/elixir-lang/elixir/releases/download/v1.9.1/Precompiled.zip -o /tmp/Precompiled.zip
 buildah run "${newcontainer}" -- unzip /tmp/Precompiled.zip -d /opt/elixir
+buildah run "${newcontainer}" -- rpm -Uhv https://rpmfind.net/linux/fedora/linux/updates/29/Everything/x86_64/Packages/i/inotify-tools-3.14-16.fc29.x86_64.rpm
 
 # set env
 buildah config --env PATH=$PATH:/opt/elixir/bin "${newcontainer}"
@@ -32,6 +33,7 @@ buildah config --env LC_ALL=en_US.utf8 "${newcontainer}"
 
 # install phoenix
 buildah run "${newcontainer}" -- mix local.hex --force
+buildah run "${newcontainer}" -- mix archive.install hex phx_new --force
 
 # Clean up yum cache
 if [ -d "${scratchmnt}" ]; then
